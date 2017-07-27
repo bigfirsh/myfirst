0e73f8c5a2a84a9f4489a25d2ef861a6462a006e
The Young Gentleman <zhaiyuqiao_qt@163.com>
2017-07-27 17:38:42 +0800

    这次是我自己的项目，你能不能给点面子

---
+++ src/com/hqhx/test/Hello.java
@@ -0,0 +1,27 @@
+package com.hqhx.test;
+
+public class Hello {
+
+//    private Integer id;
+//    private String name;
+//
+//    public Hello() {
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Hello{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                '}';
+//    }
+
+//    public Hello(Integer id, String name) {
+//        this.id = id;
+//        this.name = name;
+//    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/com/hqhx/test/World.java
@@ -0,0 +1,8 @@
+package com.hqhx.test;
+
+public class World {
+    public static void main(String[] args) {
+        System.out.println("Hello World One More Time");
+    }
+
+}
